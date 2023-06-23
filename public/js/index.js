6423f31d15168cc4f4b2c6b449a7634b7d36288b
jefflangtech <jeff.m.lang@gmail.com>
2023-06-22 20:40:50 -0700

    Completed visual functionality for the rating scores bubbles

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,28 @@
 // Use the .selected class to toggle when a bubble is clicked for the styling
+const getParent = function(elements) {
+
+  let parents = [];
+  for(let el of elements) {
+    parents.push(el.parentElement);
+  }
+
+  return parents;
+
+};
+
+const ratingDiv = document.getElementById('rater');
+const ratingScores = document.querySelectorAll('input[name=rating]');
+
+ratingDiv.addEventListener('click', (event) => {
+
+  event.preventDefault();
+
+  let elParents = getParent(ratingScores);
+
+  for(let el of elParents) {
+    if(el.contains(event.target)) {
+      el.classList.toggle('bubble-selected');
+    }
+  }
+
+});
