605e5206d63cff4c37d31261d74e809161b2db1d
jefflangtech <jeff.m.lang@gmail.com>
2023-06-24 10:35:38 -0700

    Created functionality for the rating bubbles and the radio inputs

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,57 @@
-// Use the .selected class to toggle when a bubble is clicked for the styling
-const getParent = function(elements) {
+const ratingDiv = document.getElementById('rater');
+const ratingsBubbles = document.querySelectorAll('.rating-score');
 
-  let parents = [];
-  for(let el of elements) {
-    parents.push(el.parentElement);
+const ratings = document.querySelectorAll('input[type="radio"]');
+
+
+// Search an array of elements and return a match if it is the event target
+// or return false. Useful for event delegation.
+const findEventTarget = function(e, elementArray) {
+
+  for(let el of elementArray) {
+    if(el.contains(e.target)) {
+      return el;
+    }
   }
 
-  return parents;
+  return false;
 
 };
 
-const ratingDiv = document.getElementById('rater');
-const ratingScores = document.querySelectorAll('input[name=rating]');
+// Look at the children for an element and return a child if it is an input
+// of the requested type. This could be set up as a recursive function.
+const findChildInput = function(parentEl, type) {
+
+  let inputs = parentEl.getElementsByTagName('input');
+
+  for(let input of inputs) {
+    if(input.type === type) {
+      return input;
+    }
+  }
+
+  return false;
+
+};
+
 
 ratingDiv.addEventListener('click', (event) => {
 
   event.preventDefault();
 
-  let elParents = getParent(ratingScores);
+  let el = findEventTarget(event, ratingsBubbles);
 
-  for(let el of elParents) {
-    if(el.contains(event.target)) {
-      el.classList.toggle('bubble-selected');
+  if(el) {
+    for(let item of ratingsBubbles) {
+      if(item != el) {
+        item.classList.remove('bubble-selected');
+      }
+    }
+    el.classList.toggle('bubble-selected');
+
+    let input = findChildInput(el, 'radio');
+    if(input) {
+      input.checked = true;
     }
   }
 
